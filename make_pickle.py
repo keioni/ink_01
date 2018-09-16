57b7354c1c172ec3c91bd390a31c4da63c698f2a
keys <keonimar@gmail.com>
2018-09-17 00:26:33 +0900

    add pickle maker

--- make_pickle.py
+++ make_pickle.py
@@ -7,33 +7,45 @@ import json
 import sys
 
 
-def make_setting_pickle(json_file: str) -> bool:
-    pickle_file = json_file.replace('.json', '.pickle')
-    pickle_file_mtime = 0
-    json_file_mtime = os.path.getmtime(json_file)
-    if os.path.exists(pickle_file):
-        pickle_file_mtime = os.path.getmtime(pickle_file)
-    if json_file_mtime > pickle_file_mtime:
-        with open(json_file, 'rb') as fp:
-            conf = json.load(fp)
-        with open(pickle_file, 'wb') as fp:
-            pickle.dump(conf, fp)
+def make_conf_pickle(conf_file: str) -> bool:
+    pickle_file = conf_file.replace('.json', '.pickle')
+    with open(conf_file, 'rb') as fpr:
+        conf = json.load(fpr)
+    if conf:
+        with open(pickle_file, 'wb') as fpw:
+            pickle.dump(conf, fpw)
             return True
     return False
 
+def get_conf_filename() -> tuple:
+    conf_file = ''
+    src = ''
+    if len(sys.argv) > 1:
+        conf_file = sys.argv[1]
+        src = 'argument'
+    if not conf_file:
+        conf_file = os.environ.get('INK_conf_file')
+        src = 'OS envvar'
+    if not conf_file:
+        conf_dir = os.path.dirname(__file__)
+        conf_file = os.path.abspath(conf_dir + '/var/settings.json')
+        src = 'hardcoded'
+    return (conf_file, src)
 
 if __name__ == '__main__':
-    json_file: str = ''
-    if sys.argv:
-        pass
-        # XXX
+    print('== Pickle Maker ==')
+    exit_code = 1
+    conf_file, src = get_conf_filename()
+    if not conf_file:
+        print('Cannot get conf filename.')
     else:
-        json_file = os.environ.get('INK_json_FILE')
-        if not json_file:
-            json_dir = os.path.dirname(__file__) + '/../..'
-            json_file = os.path.abspath(json_dir + '/var/settings.json')
-        if not json_file:
-            msg = 'Cannot load default settings. Retry with filename.'
-            raise ValueError(msg)
-
-    make_setting_pickle(json_file)
+        print('Conf file by {}: {}'.format(src, conf_file))
+        if os.path.exists(conf_file):
+            if make_conf_pickle(conf_file):
+                print('Succeeded!')
+                exit_code = 0
+            else:
+                print('Failed: making pickle failed.')
+        else:
+            print('Failed: specified file not found.')
+    sys.exit(exit_code)
