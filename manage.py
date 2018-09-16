0a68becde190340b27115aeac0519c7aad5571f9
keys <keonimar@gmail.com>
2018-09-16 19:34:50 +0900

    modify manager for new config system

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import os
 from ink.sys.config import CONF
 
 def hoge1():
-    conf_file = os.environ.get('INK_CONF_FILE')
-    if not conf_file:
-        conf_file = './var/settings.json'
     CONF.load()
-#    CONF.load(conf_file)
     print(CONF)
     print(CONF.database)
     print(CONF.database.host)
