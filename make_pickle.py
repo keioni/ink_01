4f64cfdf6691d4a84b466e1fcc7a3d49742f58a9
keys <keonimar@gmail.com>
2018-09-24 14:14:40 +0900

    more refactor pickle maker

--- make_pickle.py
+++ make_pickle.py
@@ -12,10 +12,9 @@ if __name__ == '__main__':
     conf_file = ''
     pickle_file = ''
 
+    # get source file (.json)
     if len(sys.argv) > 1:
         conf_file = sys.argv[1]
-        if len(sys.argv) > 2:
-            pickle_file = sys.argv[2]
     else:
         if 'INK_CONF_FILE' in os.environ:
             conf_file = os.environ.get('INK_CONF_FILE')
@@ -23,9 +22,13 @@ if __name__ == '__main__':
             conf_dir = os.path.dirname(__file__)
             conf_file = os.path.abspath(conf_dir + '/var/settings.json')
 
-    if not pickle_file:
+    # get destination file (.pickle)
+    if len(sys.argv) > 2:
+        pickle_file = sys.argv[2]
+    else:
         pickle_file = conf_file.replace('.json', '.pickle')
 
+    # read and write
     with open(conf_file, 'rb') as fpr:
         conf = json.load(fpr)
     print('Conf file: {}'.format(conf_file))
