2a41fcb6d7118adaa5a071f76c7003adaa89d971
keys <keonimar@gmail.com>
2018-09-29 23:57:45 +0900

    divide maintainer to functions by command

--- maintain.py
+++ maintain.py
@@ -18,39 +18,51 @@ class ConnectorMock:
             print('execute> ' + stmt )
 
 
-if __name__ == '__main__':
-    CONF.load()
-    progname = sys.argv.pop(0)
-    cmd = sys.argv.pop(0)
-    args = dict()
-    for i in range(len(sys.argv)):
-        args[i] = sys.argv[i]
+def mp():
+    print('>> Pickle Maker starting...')
+    ink.util.make_pickle(args.get(0), args.get(1))
+    print('>> Pickle Maker finished.')
 
-    if cmd == 'mp':
-        print('>> Pickle Maker starting...')
-        ink.util.make_pickle(args.get(0), args.get(1))
-        print('>> Pickle Maker finished.')
-    elif cmd == 'dbm':
-        if args.get(0, 'dry') == 'run':
-            db_connector = Connector(CONF.database.connect_string)
-        else:
-            db_connector = ConnectorMock()
-        dbm = ink.util.DBMaintainer(db_connector)
-        tables = dbm.get_defined_tables()
-        print(json.dumps(tables, indent=4))
-    elif cmd == 't_dbm':
-        if args.get(0, 'dry') == 'run':
-            db_connector = Connector(CONF.database.connect_string)
-        else:
-            db_connector = ConnectorMock()
-        dbm = ink.util.DBMaintainer(db_connector)
-        tables1 = dbm.get_defined_tables('tests/test_table_schema1.sql')
-        tables2 = dbm.get_defined_tables('tests/test_table_schema2.sql')
-        print(json.dumps(tables1, indent=4))
-        print(json.dumps(tables2, indent=4))
-    elif cmd == 'cc':
-        print(CONF)
-        print(CONF.database)
-        print(CONF.database.connect_string.host)
+def dbm():
+    if args.get(0, 'dry') == 'run':
+        db_connector = Connector(CONF.database.connect_string)
     else:
-        print('Bad command: {}'.format(cmd))
+        db_connector = ConnectorMock()
+    dbm = ink.util.DBMaintainer(db_connector)
+    tables = dbm.get_defined_tables()
+    print(json.dumps(tables, indent=4))
+
+def t_dbm():
+    if args.get(0, 'dry') == 'run':
+        db_connector = Connector(CONF.database.connect_string)
+    else:
+        db_connector = ConnectorMock()
+    dbm = ink.util.DBMaintainer(db_connector)
+    tables1 = dbm.get_defined_tables('tests/test_table_schema1.sql')
+    tables2 = dbm.get_defined_tables('tests/test_table_schema2.sql')
+    print(json.dumps(tables1, indent=4))
+    print(json.dumps(tables2, indent=4))
+
+def cc():
+    print(CONF)
+    print(CONF.database)
+    print(CONF.database.connect_string.host)
+
+
+CONF.load()
+progname = sys.argv.pop(0)
+cmd = sys.argv.pop(0)
+args = dict()
+for i in range(len(sys.argv)):
+    args[i] = sys.argv[i]
+
+if cmd == 'mp':
+    mp()
+elif cmd == 'dbm':
+    dbm()
+elif cmd == 't_dbm':
+    t_dbm()
+elif cmd == 'cc':
+    cc()
+else:
+    print('Bad command: {}'.format(cmd))
