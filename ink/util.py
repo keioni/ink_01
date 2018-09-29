e48b197f02c366ba38453776fa0fc7e0c2745b86
keys <keonimar@gmail.com>
2018-09-29 19:33:29 +0900

    write code but not tested

--- ink/util.py
+++ ink/util.py
@@ -4,6 +4,7 @@ import os
 import pickle
 import json
 import sys
+import re
 from hashlib import blake2b
 from base64 import b64encode
 
@@ -19,8 +20,6 @@ def vp(msg: str):
     else:
         return
 
-    if verbose_level == 0:
-        return
     if verbose_level > 1:
         print('> ' + msg, file=sys.stderr)
     if verbose_level > 2:
@@ -37,83 +36,59 @@ class DBMaintainer:
     # select * from cards where user_id=$UID and box_id=$BID
     # select record from records where card_id=$CID
 
-    TABLE_DEFS = {
-    'tokens': '''
-        create table tokens (
-            rowid integer auto_increment,
-            token bigint,
-            user_id integer,
-            ctime datetime not null,
-            primary key (token),
-        )
-    ''',
-    'users': '''
-        create table users (
-            rowid integer auto_increment,
-            token bigint,
-            user_id integer,
-            ctime datetime not null,
-            primary key (token),
-        )
-    ''',
-    'boxes': '''
-        create table boxes (
-            box_id integer auto_increment,
-            user_id integer,
-            box_title varchar(32) not null,
-            ctime datetime,
-            mtime datetime,
-            primary key (box_id),
-            index index_user (user_id),
-        )
-    ''',
-    'cards': '''
-        create table cards (
-            card_id integer auto_increment,
-            box_id integer,
-            user_id integer,
-            card_title varchar(32) not null,
-            ctime datetime,
-            mtime datetime,
-            recent_records char(340),
-            primary key (card_id),
-            index index_user_box (user_id, box_id),
-        )
-    ''',
-    'records': '''
-        create table records (
-            rowid integer auto_increment,
-            card_id integer,
-            record char(16),
-            primary key (rowid),
-            index index_card (card_id),
-        )
-    '''
-    }
-
     def __init__(self):
-        self.dbc = ink.sys.database.connect(CONF.database)
+        self.dbc = ink.sys.database.connect(CONF.database.connect_string)
 
     def __get_defined_tables(self) -> list:
-        tables = list()
-        for table_name in self.TABLE_DEFS.keys():
-            tables.append(table_name)
-        return tables
+        table_dict = dict()
+        with open(CONF.database.schema_file, 'r') as fpr:
+            table_name = str()
+            lines = list()
+            for line in fpr:
+                # remove comment
+                comment_start = line.find('#')
+                if comment_start >= 0:
+                    line = line[:comment_start]
 
-    def create_tables(self, tables: list=None) -> bool:
+                # check blank line or not.
+                if line == '':
+                    if table_name:
+                        # end of table definition.
+                        table_dict[table_name] = ' '.join(lines)
+                        lines.clear()
+                        table_name = ''
+                else:
+                    # start of table definition
+                    stmt_begin = re.match(r'create table (\w+)', line)
+                    if stmt_begin:
+                        if table_name:
+                            table_dict[table_name] = ' '.join(lines)
+                            lines.clear()
+                        table_name = stmt_begin.group(0)
+
+                if table_name:
+                    lines.append(line)
+
+            table_list = list()
+            for statement in table_dict.values():
+                table_list.append(statement)
+
+        return table_list
+
+    def create_tables(self, tables: dict=None) -> bool:
         if not tables:
             tables = self.__get_defined_tables()
         statements = list()
-        for table in tables:
-            statements.append(self.TABLE_DEFS[table])
+        for table_name in tables.keys():
+            statements.append(tables[table_name])
         return self.dbc.execute(statements)
 
-    def destroy_tables(self, tables: list=None) -> bool:
+    def destroy_tables(self, tables: dict=None) -> bool:
         if not tables:
             tables = self.__get_defined_tables()
         statements = list()
-        for table in tables:
-            statements.append('drop table {}'.format(table))
+        for table_name in tables.keys():
+            statements.append('drop table {}'.format(table_name))
         return self.dbc.execute(statements)
 
 
