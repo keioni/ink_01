a2fe215b97a37b35dd1d93ad41c847c26c63bc7d
keys <keonimar@gmail.com>
2018-09-29 16:14:25 +0900

    merge manager.py and maintain.py

--- maintain.py
+++ maintain.py
@@ -4,13 +4,23 @@
 import sys
 
 import ink.util
+from ink.sys.config import CONF
 
 if __name__ == '__main__':
+    CONF.load()
+    progname = sys.argv.pop(0)
+    cmd = sys.argv.pop(0)
     args = dict()
     for i in range(len(sys.argv)):
         args[i] = sys.argv[i]
 
-    if args[1] == 'mp':
+    if cmd == 'mp':
         print('>> Pickle Maker starting...')
-        ink.util.make_pickle(args.get(2), args.get(3))
+        ink.util.make_pickle(args.get(0), args.get(1))
         print('>> Pickle Maker finished.')
+    elif cmd == 'cc':
+        print(CONF)
+        print(CONF.database)
+        print(CONF.database.host)
+    else:
+        print('Bad command: {}'.format(cmd))
